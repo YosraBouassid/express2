98a86f0d66aa04a5a1a10ce3038a344c3b8272d3
YosraBouassid <88270381+YosraBouassid@users.noreply.github.com>
2021-09-26 10:30:35 +0100

    Add files via upload

---
+++ server.js
@@ -0,0 +1,32 @@
+//const express = require("express")
+//const app = express()
+//app.get("/",(req,res)=>{res.sendFile(__dirname,"/public/home.html")})
+
+//app.listen(5000,()=>{console.log("server is running",5000)})
+
+
+
+const express = require("express")
+const app = express()
+// console.log(app)
+const midleware = require("./midleware")
+app.use(midleware)
+
+
+app.get("/",(req,res) => {
+    // console.log(req)
+    res.sendFile(__dirname+"/public/home.html")
+})
+app.get("/services",(req,res) => {
+    // console.log(req)
+    res.sendFile(__dirname+"/public/services.html")
+})
+app.get("/contact",(req,res) => {
+    // console.log(req)
+    res.sendFile(__dirname+"/public/contact.html")
+})
+
+
+app.listen(5000,()=>{
+    console.log("server is running",5000)
+})
